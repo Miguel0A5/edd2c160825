5f45ccc3f757563638d6749e9d0c8f1d4fb53140
MiguelA536 <2410307@alumnos.univo.edu.mx>
2025-08-16 15:35:53 -0600

    Primer lista finalizada

--- src/main/java/com/mycompany/repasoc1/Repasoc1.java
+++ src/main/java/com/mycompany/repasoc1/Repasoc1.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.repasoc1;
 
+import java.util.*;
+
+
 /**
  *
  * @author migue
@@ -11,7 +14,26 @@ package com.mycompany.repasoc1;
 public class Repasoc1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello listas !");
+
+        //Arreglo de 5 calificaciones
+        double[] calificaciones = new double[5];
+        Scanner sc = new Scanner(System.in);
+        double suma = 0;
+        
+        //Pedimos al usuario ingresar 5 calificaciones
+        for(int i = 0; i<calificaciones.length; i++){
+            System.out.println("Ingresa la calificación " + (i+1) + ": ");
+            calificaciones[i] = sc.nextDouble(); //Guardarcada calificación
+            suma+= calificaciones[i]; //Sumamos para calcular el promedio despues     
+        }
+        
+        //Calcular el promedio
+        double promedio = suma / calificaciones.length;
+        
+        //Mostrar el resultado
+        System.out.println("El promedio es: " + promedio);
+        
+        
     }
     
 }
